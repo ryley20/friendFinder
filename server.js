7fe54ec6434eecab886f472a35d393a7d60f2698
Ryley Chapman <Ryley20@yahoo.com>
2019-07-08 21:09:44 -0500

    joined path for server js

--- server.js
+++ server.js
@@ -19,3 +19,7 @@ app.use(htmlroutes);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
