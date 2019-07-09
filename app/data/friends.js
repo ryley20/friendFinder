c72e40087e76280a54a2e881898dda784d70802a
Ryley Chapman <Ryley20@yahoo.com>
2019-07-08 21:18:04 -0500

    deployment

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,21 @@
-var friends = [
+var friendsArr = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
-	},
-	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
-	},
-	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
-	},
-	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
-	},
-	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	  "name":"Ryley",
+	  "photo":"https://avatars1.githubusercontent.com/u/50000094?s=400&u=1b4926681b7e87861f5c9133a99370d3f00966cb&v=4",
+	  "scores[]":[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
 	}
 ];
 
-module.exports = friends;
+
+module.exports = friendsArr;
