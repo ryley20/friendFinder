8edb9262d74420c40b59943be8184837978b2462
Ryley Chapman <Ryley20@yahoo.com>
2019-07-08 22:47:11 -0500

    got a few links working

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.listen(PORT, function() {
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
 })
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/survey.html"));
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 })
