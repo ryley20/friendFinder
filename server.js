8f96012029009b0256508a4f55582e4231208a1d
Ryley Chapman <Ryley20@yahoo.com>
2019-07-08 20:58:13 -0500

    server javascript written for express bodyparser and path

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+var apiroutes = require("./app/routing/apiroutes.js");
+var htmlroutes = require("./app/routing/htmlroutes.js");
+
+app.use(apiroutes);
+app.use(htmlroutes);
+
+//listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
